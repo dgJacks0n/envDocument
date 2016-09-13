38a3655c5e2f6ed6d74c1624fc524c11c72a8cd1
Donald Jackson <donald.jackson@bms.com>
2016-09-13 17:55:53 -0400

    Added relative path option

--- envDocument/R/get_scriptpath.R
+++ envDocument/R/get_scriptpath.R
@@ -3,11 +3,14 @@
 #' \code{get_scriptpath} returns the full path of the script that called this function (if any)
 #' or NULL if path is not available
 #' 
+#' @param absolute (logical): Return full path (absolute = TRUE, default) or path relative
+#' to working directory
+#' 
 #' @examples 
 #' mypath <- get_scriptpath()
 #' @export
 #' 
-get_scriptpath <- function() {
+get_scriptpath <- function(absolute = TRUE) {
   # location of script can depend on how it was invoked:
   # source() and knit() put it in sys.calls()
   path <- NULL
@@ -16,14 +19,27 @@ get_scriptpath <- function() {
     # get name of script - hope this is consisitent!
     path <- as.character(sys.call(1))[2] 
     # make sure we got a file that ends in .R, .Rmd or .Rnw
-    if (grepl("..+\\.[R|Rmd|Rnw]", path, perl=TRUE, ignore.case = TRUE) )  {
-      return(path)
-    } else { 
-      message("Obtained value for path does not end with .R, .Rmd or .Rnw: ", path)
-    }
   } else{
     # Rscript and R -f put it in commandArgs
     args <- commandArgs(trailingOnly = FALSE)
+    path <- args
   }
+  
+  if ( !(grepl("..+\\.[R|Rmd|Rnw]$", path, perl=TRUE, ignore.case = TRUE)) )  {
+    warning("Obtained value for path does not end with .R, .Rmd or .Rnw: ", path)
+    return(NA)
+  }
+  
+  # expand ~ if any
+  path <- normalizePath(path)
+  
+  # if absolute path is requested then return full path
+  # otherwise return relative to working directory
+  if(!absolute) {
+    wdpath <- normalizePath(getwd())
+    
+    path <- sub(normalizePath(getwd()), ".", path, fixed = TRUE)
+  }
+  
   return(path)
 }
