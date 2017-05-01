8457f5b567a32a81ced4b9f45bf0be7c14f8cf5f
Donald Jackson (Genomics) <donald.jackson@bms.com>
2017-05-01 13:32:23 +0000

    Added run URL, format variable names

--- envDocument/R/getDominoInfo.R
+++ envDocument/R/getDominoInfo.R
@@ -8,15 +8,35 @@ getDominoInfo <- function() {
   
   domino_values <- Sys.getenv(domino_varnames)
   
-  # did we get values?
+  # Return 'not found' values if no info was found
   if(nchar(domino_values["DOMINO_RUN_ID"]) == 0) {
-    stop("No Domino Run ID found")
+    return(infoNotFound())
   }
+  
+  # strip port off of DOMINO_API_HOST to get sever
+  domino_values["DOMINO_SERVER"] <- sub(":\\d+$", "", 
+                                        domino_values["DOMINO_API_HOST"])
+  
+  # build url
+  domino_values["DOMINO_RUN_URL"] <- paste(domino_values["DOMINO_SERVER"],
+                                           "u",
+                                           domino_values["DOMINO_STARTING_USERNAME"],
+                                           domino_values["DOMINO_PROJECT_NAME"],
+                                           "runs",
+                                           domino_values["DOMINO_RUN_ID"],
+                                           sep = "/")
+  domino_values["DOMINO_RUN_URL"] <- paste0(domino_values["DOMINO_RUN_URL"], "#info")  
+
+  
   
   domino_values <- data.frame(domino_values, stringsAsFactors = FALSE)
   colnames(domino_values) <- "Value"
   
+  # format variable names
   domino_values$Name <- sub("^DOMINO_", "", rownames(domino_values))
+  domino_values$Name <- gsub("_", " ", domino_values$Name)
+  domino_values$Name <- stringr::str_to_title(domino_values$Name)
+  
   domino_values <- domino_values[c("Name", "Value")]
   rownames(domino_values) <- NULL
   
