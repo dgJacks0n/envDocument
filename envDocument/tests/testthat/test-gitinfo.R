24999c31c8c181d876ec0ba10bd8928d65d2958f
Donald Jackson <donald.jackson@bms.com>
2017-07-18 22:53:25 -0400

    Made tests conditional on git2r.  Add test for repo.

--- envDocument/tests/testthat/test-gitinfo.R
+++ envDocument/tests/testthat/test-gitinfo.R
@@ -2,5 +2,21 @@ context("gitinfo")
 
 # does git info work?
 test_that("git lookup", {
+  # skip if git2r is not available
+  if (!requireNamespace("git2r", quietly = TRUE)) {
+    skip("git2r not available")
+  }
+  
   expect_s3_class(getGitInfo("../../DESCRIPTION"), "data.frame")
+})
+
+
+test_that("finds repo", {
+  # skip if git2r is not available
+  if (!requireNamespace("git2r", quietly = TRUE)) {
+    skip("git2r not available")
+  }
+  
+  res <- getRepo("../../DESCRIPTION")
+  expect_s4_class(res, "git_repository")
 })
