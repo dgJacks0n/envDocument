73d6eb3f079a1ecffd7269245ac27729e74d824b
Donald Jackson <donald.jackson@sanofi.com>
2025-08-25 14:36:10 -0400

    R/getScrptPath.R: Add support for Quarto documents; use knitr::current_input for rmarkdown, check that path is valid

--- envDocument/R/getScriptPath.R
+++ envDocument/R/getScriptPath.R
@@ -19,27 +19,50 @@ getScriptPath <- function(absolute = TRUE) {
   path <- NULL
   
   # location of script can depend on how it was invoked:
-  # source() and knit() put it in sys.calls()
-  if(!is.null(sys.calls())) {
-    # get name of script - hope this is consistent!
-    path <- as.character(sys.call(1))[2] 
-    # make sure we got a file that ends in .R, .Rmd or .Rnw
-  } else{
-    # Rscript and R -f put it in commandArgs
-    args <- commandArgs(trailingOnly = FALSE)
-    path <- args
+  # quarto render puts it in environment variables but ONLY
+  # for versions >= 1.7
+  if(!is.na( Sys.getenv("QUARTO_DOCUMENT_FILE", NA) )) {
+    path <- file.path(Sys.getenv("QUARTO_PROJECT_ROOT"),
+                      Sys.getenv("QUARTO_DOCUMENT_PATH"),
+                      Sys.getenv("QUARTO_DOCUMENT_FILE"))
+    
+  } else  if(!is.null(knitr::current_input())) {
+    # next try knitr::current input for Rmarkdown and for Quarto versions <1.7
+    path <- knitr::current_input(dir = T)
+      # change extension if quarto project?
+      if(!is.na( Sys.getenv("QUARTO_DOCUMENT_PATH", NA))  ) {
+        path <- sub("\\.rmarkdown$", ".qmd", path)
+      }
+  } else {
+    # source() and knit() put it in sys.calls()
+    if(!is.null(sys.calls())) {
+      # get name of script - hope this is consistent!
+      path <- as.character(sys.call(1))[2] 
+    } else {
+      # Rscript and R -f put it in commandArgs
+      args <- commandArgs(trailingOnly = FALSE)
+      path <- args
+    }
   }
   
   if(is.null(path) | is.na(path)) {
     stop("No path information available.")
   }
   
-  if ( !(grepl(".+[R|Rmd|Rnw]$", path, perl=TRUE, ignore.case = TRUE)) )  {
-    stop("Obtained value for path <", path, "> does not end with .R, .Rmd or .Rnw: ", path)
+  # make sure we got a file that ends in .R, .Rmd or .Rnw
+  if ( !(grepl(".+[R|Rmd|Rnw|Qmd]$", path, perl=TRUE, ignore.case = TRUE)) )  {
+    stop("Obtained value for path <", path, 
+         "> does not end with .R, .Rmd, .Qmd or .Rnw: ", path)
   }
   
   # expand ~ if any
   path <- normalizePath(path, winslash = "/")
+  
+  # check that file exists
+  if( !(file.exists(path))) {
+    stop("Obtained value for path <", path,
+         "> does not exist")
+  }
   
   # if absolute path is requested then return full path
   # otherwise return relative to working directory
