84f7d3ba55ca62953d256d38abea969a28becb63
Donald Jackson <donald.jackson@sanofi.com>
2025-09-08 16:16:47 -0400

    tests/testthat/test-gitinfo.R: use skip_if_notinstalled instead of manually checking for git2r

--- tests/testthat/test-gitinfo.R
+++ tests/testthat/test-gitinfo.R
@@ -4,25 +4,23 @@ context("gitinfo")
 
 # does git info work?
 test_that("git lookup", {
-  # skip if git2r is not available
-  if (!requireNamespace("git2r", quietly = TRUE)) {
-    skip("git2r not available")
-  }
+
+  skip_if_not_installed("git2r")
   
   # don't run this test on CRAN
   skip_on_cran()
-  expect_is(getGitInfo("./test-gitinfo.R"), "data.frame")
+  skip_on_ci()
+  expect_is(getGitInfo(normalizePath("./test-gitinfo.R")), 
+            "data.frame")
 })
 
 
 test_that("finds repo", {
-  # skip if git2r is not available
-  if (!requireNamespace("git2r", quietly = TRUE)) {
-    skip("git2r not available")
-  }
+  skip_if_not_installed("git2r")
   
   # don't run this test on CRAN
   skip_on_cran()
-  res <- getRepo("./test-gitinfo.R")
+  skip_on_ci()
+  res <- getRepo(normalizePath("./test-gitinfo.R"))
   expect_is(res, "git_repository")
 })
