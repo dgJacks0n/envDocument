325ab7061e34539ae7a5b27202d1ef5876d9a69f
Donald Jackson <donald.jackson@sanofi.com>
2025-09-09 09:55:28 -0400

    tests/testthat/test-gitinfo.R: only run if git2r is installed and .git directory is findable

--- tests/testthat/test-gitinfo.R
+++ tests/testthat/test-gitinfo.R
@@ -5,22 +5,23 @@ context("gitinfo")
 # does git info work?
 test_that("git lookup", {
 
+  # only run if git2r is installed and .git directory exists in project root
   skip_if_not_installed("git2r")
   
-  # don't run this test on CRAN
-  skip_on_cran()
-  skip_on_ci()
+  skip_if_not(dir.exists(file.path(here::here(), ".git")))
+  
   expect_is(getGitInfo(normalizePath("./test-gitinfo.R")), 
             "data.frame")
 })
 
 
 test_that("finds repo", {
+
+  # only run if git2r is installed and .git directory exists in project root
   skip_if_not_installed("git2r")
   
-  # don't run this test on CRAN
-  skip_on_cran()
-  skip_on_ci()
+  skip_if_not(dir.exists(file.path(here::here(), ".git")))
+  
   res <- getRepo(normalizePath("./test-gitinfo.R"))
   expect_is(res, "git_repository")
 })
